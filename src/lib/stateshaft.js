04cd05c15eb1e3510ac19ed1e257dc6f4bb740f3
Nnamdi Michael Okpala <160319266+okpalandev@users.noreply.github.com>
2024-02-24 00:30:36 +0000

    Update stateshaft.js

--- src/lib/stateshaft.js
+++ src/lib/stateshaft.js
@@ -1,9 +1,7 @@
 ;(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['b'], function (b) {
-            return (root.stateshaft = factory();
-        });
+        define([],()=> (root.stateshaft = factory());
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
